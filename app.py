fde8f20895e9f134bf2d080697ee4b9b2884b869
Azar867 <52555238+Azar867@users.noreply.github.com>
2023-08-15 21:41:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, render_template, request, jsonify
+import openai
 
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-
-
-tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
-
+openai.api_key = "ENTER_YOUR_OPEN_API_KEY"
 
 app = Flask(__name__)
 
@@ -20,24 +14,17 @@ def index():
 def chat():
     msg = request.form["msg"]
     input = msg
-    return get_Chat_response(input)
+    chat_messages = [{'role': 'system', 'content': 'Welcome! How I can help you?'}, {'role': 'user', 'content': input}]
+    return get_openai_response(chat_messages)
 
-
-def get_Chat_response(text):
-    # Let's chat for 5 lines
-    for step in range(5):
-        # encode the new user input, add the eos_token and return a tensor in Pytorch
-        new_user_input_ids = tokenizer.encode(str(text) + tokenizer.eos_token, return_tensors='pt')
-
-        # append the new user input tokens to the chat history
-        bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-
-        # generated a response while limiting the total chat history to 1000 tokens, 
-        chat_history_ids = model.generate(bot_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id)
-
-        # pretty print last ouput tokens from bot
-        return tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
-
+def get_openai_response(messages):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=100,
+    )
+    print("response :: ",response)
+    return response['choices'][0]['message']['content']
 
 if __name__ == '__main__':
     app.run()
